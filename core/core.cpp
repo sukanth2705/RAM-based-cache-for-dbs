3157c73ad8b6ad0e296a8c939acfca67aff07939
C Pranav Vignesh <pranavvignesh315@gmail.com>
2024-08-04 19:37:17 +0530

    added string class

--- core/core.cpp
+++ core/core.cpp
@@ -2,13 +2,8 @@
 
 #include<iostream>
 
-core::Int::Int(int data):data(data){}
+
 int core::Int::get() {return this->data;}
 void core::Int::set(int val) {this->data=val;}
-int core::Int::test(){return this->data;}
 
-int main(){
-    core::Int v(4);
-    std::cout<<v.test()<<std::endl;
-    return 0;
-}
+
