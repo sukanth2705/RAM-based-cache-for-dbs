b3385660bd465677d5f78215d7da1d7ed7e34b05
sukanth2705 <sukanth2705@gmail.com>
2024-08-04 20:05:00 +0530

    fix string class

--- core/core.cpp
+++ core/core.cpp
@@ -14,7 +14,7 @@ namespace core{
     void Float::set(float val) {this->data=val;}
     float Float::test(){return this->data;}
 
-    String::String(std::string data):data(data){}
+    String::String(std::string data):data(data),Record(STRING){}
     std::string String::get() {return this->data;}
     void String::set(std::string val) {this->data=val;}
     std::string String::test(){return this->data;}
@@ -23,7 +23,9 @@ namespace core{
 int main(){
     core::Int v(4);
     core::Float f(3.5);
+    core::String s("abcd");
     std::cout<<v.test()<<std::endl;
     std::cout<<f.test()<<std::endl;
+    std::cout<<s.test()<<std::endl;
     return 0;
 }
