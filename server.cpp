bdf1d2ee25637866bb6c0d624335b776abc4507f
sukanth2705 <sukanth2705@gmail.com>
2024-08-10 00:38:39 +0530

    add this as a service

--- server.cpp
+++ server.cpp
@@ -15,16 +15,16 @@ int get_elapsed_seconds(Record<int> *record)
     return elapsed_seconds.count();
 }
 
-std::vector<std::string> randomSample(Cache *c)
+std::vector<std::string> randomSample(Cache *db)
 {
     std::vector<std::string> sample;
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, c->num_keys - 1);
-    std::vector<int> indices(c->num_keys);
+    std::uniform_int_distribution<> dis(0, db->num_keys - 1);
+    std::vector<int> indices(db->num_keys);
     std::iota(indices.begin(), indices.end(), 0);
     std::shuffle(indices.begin(), indices.end(), gen);
-    for (auto it = c->keys.begin(); it != c->keys.end(); ++it)
+    for (auto it = db->keys.begin(); it != db->keys.end(); ++it)
     {
         sample.push_back(*it);
     }
@@ -32,24 +32,12 @@ std::vector<std::string> randomSample(Cache *c)
     return sample;
 }
 
-bool readFileChecker(std::ifstream &fileObject) // function to check if file object is opened properly
-{
-    if (fileObject)
-        return true;
-    else
-    {
-        std::cerr << "Unable to reconstruct Cache as log files are not opening";
-        fileObject.close();
-        return false;
-    }
-}
-
-void persistance(Cache *c, std::string operated_key)
+void persistance(Cache *db, std::string operated_key)
 {
     std::string absolute_log_file_path = std::string(FLAGS_log_path) + "/" + std::string(FLAGS_log_file);
     std::ofstream log;
-    log.open(absolute_log_file_path, std::ios::app); // opening log file to append command
-    Record<int> *operated_record = dynamic_cast<Record<int> *>(c->get(operated_key));
+    log.open(absolute_log_file_path, std::ios::app);
+    Record<int> *operated_record = dynamic_cast<Record<int> *>(db->get(operated_key));
     int time_elapsed = get_elapsed_seconds(operated_record);
     if (log && time_elapsed < operated_record->ttl)
     {
@@ -62,47 +50,47 @@ void persistance(Cache *c, std::string operated_key)
     log.close();
 }
 
-void cleaner(Cache *c)
+void cleaner(Cache *db)
 {
     while (1)
     {
         std::this_thread::sleep_for(std::chrono::seconds(FLAGS_cleaner_timeout));
-        std::vector<std::string> sample_keys = randomSample(c);
+        std::vector<std::string> sample_keys = randomSample(db);
         std::chrono::time_point<std::chrono::system_clock> currenttimestamp = std::chrono::system_clock::now();
         for (int i = 0; i < sample_keys.size(); i++)
         {
-            const auto record = dynamic_cast<Record<int> *>(c->table[sample_keys[i]]);
+            const auto record = dynamic_cast<Record<int> *>(db->table[sample_keys[i]]);
             if (get_elapsed_seconds(record) >= record->ttl)
             {
-                c->table.erase(sample_keys[i]);
-                c->num_keys--;
-                c->keys.erase(sample_keys[i]);
+                db->table.erase(sample_keys[i]);
+                db->num_keys--;
+                db->keys.erase(sample_keys[i]);
             }
         }
     }
 }
 
-void master(Cache *c)
+void master(Cache *db)
 {
     Record<int> v1(3, 10), v2(4), v3(5);
-    c->set("a", &v1);
-    persistance(c, "a");
-    c->set("b", &v2);
-    persistance(c, "b");
-    c->set("c", &v3);
-    persistance(c, "c");
+    db->set("a", &v1);
+    persistance(db, "a");
+    db->set("b", &v2);
+    persistance(db, "b");
+    db->set("c", &v3);
+    persistance(db, "c");
     while (1)
     {
         std::cout << "master:";
-        std::cout << c->num_keys << std::endl;
+        std::cout << db->num_keys << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 }
 
-void initialize(Cache *c)
+void initialize(Cache *db)
 {
-    std::thread master_th(master, c);
-    std::thread cleaner_th(cleaner, c);
+    std::thread master_th(master, db);
+    std::thread cleaner_th(cleaner, db);
     master_th.join();
     cleaner_th.join();
 }
---
+++ service.cpp
@@ -0,0 +1,11 @@
+#include "cache/server.h"
+
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    Cache db;
+    initialize(&db);
+    std::cout<<"Service initialized and started .....\n";
+}
